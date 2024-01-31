1d72f13eb5ff8781e198532e08e31969d1f465c0
SAMUEL AKINOLA ELUYEFA <149276078+MRKIKSY@users.noreply.github.com>
2024-01-31 21:28:14 +0000

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ module.exports = {
       return [
         {
           source: '/register',
-          destination: '/',
+          destination: '/register',
           permanent: true,
         },
       ]
